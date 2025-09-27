49c39dbd17160cebd745fb3a9f12e816b9c921a8
MandarKanbargi <mandar.kanbargi24.com>
2025-09-27 19:31:47 +0530

    finalone

--- src/components/pages/slot-selection.tsx
+++ src/components/pages/slot-selection.tsx
@@ -347,7 +347,7 @@ const AppHeaderWithBack = ({ onBack }: { onBack: () => void }) => (
             className="border-background-300"
           >
             <Icons.arrowLeft className="w-4 h-5 mr-1" />
-          <div className="w-8 h-8 rounded-lg flex items-center justify-center">
+         
             
             <Icons.home className="w-5 h-5 text-primary-200 fallback-icon hidden" />
           </div>
